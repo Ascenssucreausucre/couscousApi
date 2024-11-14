cd2751ec831382c9006b5ce8f0d8d7a81149c786
AMIOT FLORIAN <famiot@iut.local>
2024-11-14 16:26:44 +0100

    woops

--- public/index.php
+++ public/index.php
@@ -1,3 +1,12 @@
 <?php
+if($_SERVER['REQUEST_METHOD'] == 'OPTIONS'){
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    header("Content-Type: application/json");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS");
 
+    http_response_code(200);
+
+    exit();
+}
 require '../src/bootstrap.php'; ?>
